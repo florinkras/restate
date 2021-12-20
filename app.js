267f6e59c8454d337f6e9745d3675ebc7b96f605
Florin Krasniqi <florinkrasniqi@Florins-MacBook-Air.local>
2021-12-20 23:38:14 +0100

    fix import outisde module error, add login button to mobile navigation on all screens

--- app.js
+++ app.js
@@ -15,47 +15,50 @@ window.addEventListener("resize", () => {
   }
 });
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let error, errorMessages;
+if (form) {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let error, errorMessages;
 
-  if (form.id === "login") {
-    const { error: loginError, errorMessages: loginErrorMessages } =
-      validateLogin();
-    error = loginError;
-    errorMessages = loginErrorMessages;
-  }
-
-  if (form.id === "signup") {
-    const { error: signupError, errorMessages: signupErrorMessages } =
-      validateSignup();
-    error = signupError;
-    errorMessages = signupErrorMessages;
-  }
-
-  if (error) {
-    const previousErrorsContainer = document.querySelector("#error-container");
-
-    const errorsContainer =
-      previousErrorsContainer || document.createElement("div");
-    errorsContainer.id = "error-container";
-    errorsContainer.className =
-      "full-width p-1 my-1 border-error bg-error text-error";
-
-    const errorParagraphs = errorMessages.map((errorMessage) => {
-      const errorElement = document.createElement("p");
-      errorElement.innerText = errorMessage;
-      return errorElement;
-    });
-
-    errorsContainer.replaceChildren(...errorParagraphs);
-
-    if (!previousErrorsContainer) {
-      form.appendChild(errorsContainer);
+    if (form.id === "login") {
+      const { error: loginError, errorMessages: loginErrorMessages } =
+        validateLogin();
+      error = loginError;
+      errorMessages = loginErrorMessages;
     }
 
-    return;
-  }
+    if (form.id === "signup") {
+      const { error: signupError, errorMessages: signupErrorMessages } =
+        validateSignup();
+      error = signupError;
+      errorMessages = signupErrorMessages;
+    }
 
-  form.removeChild(document.querySelector("#error-container"));
-});
+    if (error) {
+      const previousErrorsContainer =
+        document.querySelector("#error-container");
+
+      const errorsContainer =
+        previousErrorsContainer || document.createElement("div");
+      errorsContainer.id = "error-container";
+      errorsContainer.className =
+        "full-width p-1 my-1 border-error bg-error text-error";
+
+      const errorParagraphs = errorMessages.map((errorMessage) => {
+        const errorElement = document.createElement("p");
+        errorElement.innerText = errorMessage;
+        return errorElement;
+      });
+
+      errorsContainer.replaceChildren(...errorParagraphs);
+
+      if (!previousErrorsContainer) {
+        form.appendChild(errorsContainer);
+      }
+
+      return;
+    }
+
+    form.removeChild(document.querySelector("#error-container"));
+  });
+}
